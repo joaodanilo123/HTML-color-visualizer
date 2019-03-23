d870f97e25f017bf4a2da9d81f2ae376c5278cf2
joao <jddiedrich@gmail.com>
2019-03-23 00:10:12 -0300

    all code to translated to english

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var cor = document.getElementById('cor')
+var colorDisplay = document.getElementById('colorDisplay')
 var red = document.getElementById('r')
 var green = document.getElementById('g')
 var blue = document.getElementById('b')
@@ -6,7 +6,7 @@ var blue = document.getElementById('b')
 function updateColor(r, g, b) {
     let color = "rgb(" + r.value + "," + g.value + "," + b.value + ")"
 
-    cor.style.backgroundColor = color
+    colorDisplay.style.backgroundColor = color
 
     console.log(color)
 }
