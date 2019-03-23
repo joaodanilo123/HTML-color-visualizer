b8678100a7279f67df012d72499869726ecd4ed1
joao <jddiedrich@gmail.com>
2019-03-23 00:08:17 -0300

    improved color update performance and smooth

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ var red = document.getElementById('r')
 var green = document.getElementById('g')
 var blue = document.getElementById('b')
 
-async function updateColor(r,g,b) {
-    let color = "rgb("+r.value+","+ g.value +","+b.value+")"
+function updateColor(r, g, b) {
+    let color = "rgb(" + r.value + "," + g.value + "," + b.value + ")"
 
     cor.style.backgroundColor = color
 
     console.log(color)
 }
-
-setInterval(updateColor,1,red,green,blue)
-
